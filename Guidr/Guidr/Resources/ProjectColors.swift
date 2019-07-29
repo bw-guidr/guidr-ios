1b85d722a5e9aae68b197c1efe0246f9718668f8
Marlon <marmoneymar@icloud.com>
2019-07-29 15:22:24 -0400

    Fixed merge conflicts, added storyboards and icons

---
+++ Guidr/Guidr/Resources/ProjectColors.swift
@@ -0,0 +1,31 @@
+//
+//  ProjectColors.swift
+//  Guidr
+//
+//  Created by Marlon Raskin on 7/29/19.
+//  Copyright © 2019 Sean Acres. All rights reserved.
+//
+
+import UIKit
+
+
+extension UIColor {
+
+	// MARK: - Neutral Palette
+
+	static let grey = UIColor(red: 67/255, green: 67/255, blue: 67/255, alpha: 1)
+	static let offWhite = UIColor(red: 242/255, green: 242/255, blue: 242/255, alpha: 1)
+
+	// MARK: - Primary Palette
+
+	static let mainGreen = UIColor(red: 88/255, green: 115/255, blue: 105/255, alpha: 1)
+	static let mainPeach = UIColor(red: 242/255, green: 193/255, blue: 132/255, alpha: 1)
+
+	// MARK: - Secondary Palette
+
+	static let secondaryGreen = UIColor(red: 143/255, green: 165/255, blue: 129/255, alpha: 1)
+	static let secondaryPeach = UIColor(red: 242/255, green: 203/255, blue: 180/255, alpha: 1)
+
+}
+
+
