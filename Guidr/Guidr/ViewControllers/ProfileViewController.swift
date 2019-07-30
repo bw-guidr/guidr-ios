4ae372fc248580c8c6feb8a16be1b36e4be62257
Marlon <marmoneymar@icloud.com>
2019-07-30 02:43:37 -0400

    Layout for profile view is just about done
    Used a collection view controller instead

--- Guidr/Guidr/ViewControllers/ProfileViewController.swift
+++ Guidr/Guidr/ViewControllers/ProfileViewController.swift
@@ -2,32 +2,87 @@
 //  ProfileViewController.swift
 //  Guidr
 //
-//  Created by Marlon Raskin on 7/29/19.
+//  Created by Marlon Raskin on 7/30/19.
 //  Copyright © 2019 Sean Acres. All rights reserved.
 //
 
 import UIKit
 
-class ProfileViewController: UIViewController {
+class ProfileViewController: UICollectionViewController {
 
-	@IBOutlet weak var tableView: UITableView!
+	var tourPhotos: [UIImage] = [UIImage(named: "bearVector")!, UIImage(named: "hikeVector")!, UIImage(named: "MountainRedVector")!, UIImage(named: "treeAndMoonVector")!, UIImage(named: "MountainVector")!]
 
-	override func viewDidLoad() {
+	let miles = "272"
+	let tours = "15"
+	let locations = "9"
+
+
+
+    override func viewDidLoad() {
         super.viewDidLoad()
-
-        tabBarController?.tabBar.tintColor = .mainPeach
-		tableView.separatorStyle = .none
+		navigationController?.navigationBar.barTintColor = .black
+		tabBarController?.tabBar.tintColor = .mainPeach
     }
-    
 
-    /*
+
     // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
+    }
+
+
+    // MARK: UICollectionViewDataSource
+
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 3
+    }
+
+
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+		switch section {
+		case 0:
+			return 1
+		default:
+			return tourPhotos.count
+		}
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+
+		switch indexPath.section {
+		case 0:
+			return guideInfoCell(from: collectionView.dequeueReusableCell(withReuseIdentifier: "GuideInfoCell", for: indexPath), atIndex: indexPath.item)
+		default:
+			return tourPhotoCell(from: collectionView.dequeueReusableCell(withReuseIdentifier: "TourPhotoCell", for: indexPath), atIndex: indexPath.item)
+		}
+    }
+
+	private func guideInfoCell(from cell: UICollectionViewCell, atIndex index: Int) -> GuideInfoCollectionViewCell {
+		guard let cell = cell as? GuideInfoCollectionViewCell else { return GuideInfoCollectionViewCell() }
+		cell.milesCountLabel.text = miles
+		cell.locationsCountLabel.text = locations
+		cell.nameLabel.text = "Emilio Montoya"
+		cell.profileImageView.image = UIImage(named: "profilePhoto")
+
+		return cell
+	}
+
+	private func tourPhotoCell(from cell: UICollectionViewCell, atIndex index: Int) -> TourPhotoCollectionViewCell {
+		guard let cell = cell as? TourPhotoCollectionViewCell else { return TourPhotoCollectionViewCell() }
+		cell.tourImageView.backgroundColor = UIColor(hue: CGFloat.random(in: 0...1), saturation: 0.8, brightness: 0.7, alpha: 1)
+
+		return cell
+	}
+}
+
+extension ProfileViewController: UICollectionViewDelegateFlowLayout {
+	func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+		switch indexPath.section {
+		case 0:
+			return CGSize(width: view.bounds.width, height: 600)
+		default:
+			return CGSize(width: 110, height: 110)
+		}
+	}
 }
