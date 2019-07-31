47be6f061f1ba55ab08e4cfbeddd4356719cedc0
Marlon <marmoneymar@icloud.com>
2019-07-31 14:26:57 -0400

    imagePicker now populates imageView
    Also added UI elements for CreateTour page

--- Guidr/Guidr/ViewControllers/CreateTourViewController.swift
+++ Guidr/Guidr/ViewControllers/CreateTourViewController.swift
@@ -50,12 +50,42 @@ class CreateTourViewController: UIViewController {
 	
 	override func viewDidLoad() {
         super.viewDidLoad()
-        
-        imageView.layer.cornerRadius = 8
 
+		imagePickerController.delegate = self
+		locationTextField.delegate = self
+		milesTextField.delegate = self
+		summaryTextView.delegate = self
+		locationTextField.becomeFirstResponder()
+
+        imageView.layer.cornerRadius = 8
+		addTourButton.layer.cornerRadius = 8
+		addTourButton.backgroundColor = .mainPeach
+		addTourButton.tintColor = .grey
+		chooseDateButton.layer.borderWidth = 2
+		chooseDateButton.layer.borderColor = UIColor.mainPeach.cgColor
+		chooseDateButton.layer.cornerRadius = 6
+		choosePhotoButton.layer.borderWidth = 2
+		choosePhotoButton.layer.borderColor = UIColor.mainPeach.cgColor
+		choosePhotoButton.layer.cornerRadius = 6
+		summaryTextView.layer.cornerRadius = 8
     }
 
-	
+	@IBAction func clearAllTapped(_ sender: UIBarButtonItem) {
+		let alert = UIAlertController(title: "Are you sure you want to clear all fields", message: nil, preferredStyle: .alert)
+		alert.addAction(UIAlertAction(title: "Clear All", style: .destructive, handler: { (clear) in
+			self.clearAll()
+		}))
+		present(alert, animated: true, completion: nil)
+	}
+
+	@IBAction func milesFormatted(_ sender: UITextField) {
+		guard let numberStr = sender.text else { return }
+		let validCharacters = Set("1234567890")
+		let sanitizedStr = numberStr.filter {validCharacters.contains($0)}
+		guard let doubleValue = Double(sanitizedStr) else { return }
+		milesTextField.text = String(format: "%.01f", doubleValue / 10)
+	}
+
 	@IBAction func segControlToggle(_ sender: UISegmentedControl) {
         if segControl.selectedSegmentIndex == 0 {
             tourType = "professional"
@@ -74,6 +104,13 @@ class CreateTourViewController: UIViewController {
 	@IBAction func addTourTapped(_ sender: UIButton) {
         addTour()
 	}
+
+	@IBAction func dismissKeyboardTapView(_ sender: UITapGestureRecognizer) {
+		locationTextField.resignFirstResponder()
+		milesTextField.resignFirstResponder()
+		summaryTextView.resignFirstResponder()
+	}
+	
     
     private func addTour() {
         guard let title = locationTextField.text,
@@ -88,6 +125,14 @@ class CreateTourViewController: UIViewController {
         
         tourController.createTour(title: title, description: description, miles: miles, date: "11/23/2019", userID: user.identifier!, imageURL: nil, location: title, tourType: tourType)
     }
+
+	private func clearAll() {
+		imageView.image = nil
+		locationTextField.text = nil
+		milesTextField.text = nil
+		summaryTextView.text = nil
+		dateLabel.text = nil
+	}
 
 
 
@@ -106,10 +151,44 @@ extension CreateTourViewController: UIImagePickerControllerDelegate {
 		imageView.contentMode = .scaleAspectFill
 		dismiss(animated: true, completion: nil)
 	}
+
+	func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+		dismiss(animated: true, completion: nil)
+	}
 }
 
 extension CreateTourViewController: DatePickerDelegate {
 	func tourDateWasChosen(date: Date) {
 		dateLabel.text = dateFormatter.string(from: date)
+	}
+}
+
+// MARK: - UINavigationControllerDelegate
+extension CreateTourViewController: UINavigationControllerDelegate {}
+
+extension CreateTourViewController: UITextViewDelegate {
+	func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+		if text == "\n" {
+			textView.resignFirstResponder()
+			return false
+		}
+		return true
+	}
+}
+
+extension CreateTourViewController: UITextFieldDelegate {
+	func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+		if let text = textField.text,
+			!text.isEmpty {
+			switch textField {
+			case locationTextField:
+				milesTextField.becomeFirstResponder()
+			case milesTextField:
+				summaryTextView.becomeFirstResponder()
+			default:
+				textField.resignFirstResponder()
+			}
+		}
+		return false
 	}
 }
