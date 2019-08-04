78812730a9e45e23b07369957619503a545c21b1
seanacres <6416690+seanacres@users.noreply.github.com>
2019-08-04 13:49:57 -0400

    Add necessary files for image upload

---
+++ Guidr/Guidr/Model Controllers/ImageController.swift
@@ -0,0 +1,71 @@
+//
+//  ImageController.swift
+//  Guidr
+//
+//  Created by Sean Acres on 8/4/19.
+//  Copyright © 2019 Sean Acres. All rights reserved.
+//
+
+import Foundation
+import CoreData
+import UIKit
+import Firebase
+
+class ImageController {
+    
+    static let shared = ImageController()
+    let imagesRef = Storage.storage().reference().child("images")
+    
+    func fetchImage(at urlString: String, completion: @escaping (Result<UIImage, NetworkError>) -> Void) {
+        guard let imageURL = URL(string: urlString) else  {
+            completion(.failure(.otherError))
+            return
+        }
+        
+        var request = URLRequest(url: imageURL)
+        request.httpMethod = HTTPMethod.get.rawValue
+        
+        URLSession.shared.dataTask(with: request) { (data, _, error) in
+            if let error = error {
+                NSLog("Error fetching image data: \(error)")
+                completion(.failure(.otherError))
+            }
+            
+            guard let data = data else {
+                completion(.failure(.badData))
+                return
+            }
+            
+            guard let image = UIImage(data: data) else {
+                completion(.failure(.badData))
+                return
+            }
+            
+            completion(.success(image))
+        }.resume()
+    }
+    
+    func uploadImage(from data: Data, imageID: UUID, completion: @escaping (Result<URL, Error>) -> Void) {
+        let uploadRef = imagesRef.child("\(imageID).jpg")
+        
+        uploadRef.putData(data, metadata: nil) { (metadata, error) in
+            if let error = error {
+                NSLog("Error uploading image: \(error)")
+                completion(.failure(error))
+                return
+            }
+            
+            uploadRef.downloadURL(completion: { (url, error) in
+                if let error = error {
+                    NSLog("Error getting download URL for image: \(error)")
+                    completion(.failure(error))
+                    return
+                }
+                
+                guard let downloadURL = url else { return }
+                
+                completion(.success(downloadURL))
+            })
+        }
+    }
+}
